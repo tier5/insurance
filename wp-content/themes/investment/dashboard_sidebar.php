40414d5d22358279224ee72be4b3e7b686e916eb
root <github@tier5.us>
2016-12-01 19:23:32 +0530

    Version 1.9 Released

--- wp-content/themes/investment/dashboard_sidebar.php
+++ wp-content/themes/investment/dashboard_sidebar.php
@@ -76,7 +76,7 @@ $('form#change_pass').on('submit', function(e) {
 							<li><a href="#"><i class="fa fa-external-link-square" aria-hidden="true"></i><span>OPT! Login</span></a></li>
 							<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i><span>Cruise Contest</span></a></li>
 							<li><a href="#"><i class="fa fa-television" aria-hidden="true"></i><span>Leaderboard</span></a></li>
-							<li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i><span>Contracting</span></a></li>
+							<li><a href="<?php echo site_url();?>/contracts/"><i class="fa fa-user-plus" aria-hidden="true"></i><span>Contracting</span></a></li>
 							<li><a href="#"><i class="fa fa-black-tie" aria-hidden="true"></i><span>Resources</span></a></li>
 							<li><a href="<?php echo site_url();?>/training"><i class="fa fa-picture-o" aria-hidden="true"></i><span>Training</span></a></li>
 							<li><a href="#"><i class="fa fa-calendar"></i><span>Events</span></a></li>
